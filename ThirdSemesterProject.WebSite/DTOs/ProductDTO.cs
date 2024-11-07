68e104cf0f71b437563781532f5f6f82e37106cc
ChristianBjerreSkadhauge <Christian.Bjerre.Skadhauge@gmail.com>
2024-11-07 10:30:24 +0100

    Created DTOs/ProductDTO Created Views/Product/List Created Views/Product/Details

---
+++ ThirdSemesterProject.WebSite/DTOs/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ThirdSemesterProject.WebSite.DTOs
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string Size { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Weight { get; set; }
+    }
+}
