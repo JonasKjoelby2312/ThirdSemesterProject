348701ca683e2a9f2ee8751c8e5d8042c7c0ae1a
JonasKjoelby2312 <150658707+JonasKjoelby2312@users.noreply.github.com>
2024-11-07 10:41:48 +0100

    Kode dokumentation

--- ThirdSemesterProject.DAL/DAOs/ProductDAOStub.cs
+++ ThirdSemesterProject.DAL/DAOs/ProductDAOStub.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 using ThirdSemesterProject.DAL.Model;
 
 namespace ThirdSemesterProject.DAL.DAOs;
-
+//Der laves en statisk liste der holder på alle produkter. 
+//Der laves 10 produkter, som tilføjes til listen. 
 public class ProductDAOStub : IDAO<Product>
 {
     private static List<Product> _products = new List<Product>() {
@@ -21,6 +22,10 @@ public class ProductDAOStub : IDAO<Product>
         new Product(9, "M", "Hiking Backpack", "A 40L hiking backpack with multiple compartments and ergonomic design.", 1.3),
         new Product(10, "One Size", "Camping Chair", "Foldable, lightweight camping chair with cup holder, perfect for relaxing by the campfire.", 1.5)
     };
+    //nextAvailableId, finder det sidste produkt i listen og plusser det fundte produkts id med 1. 
+    //Id sættes på produkt 
+    //Produktet tilføjes til listen.
+    //Id'et returnes
     public int Create(Product entity)
     {
         var nextAvailableId = _products.Max(entity => entity.ProductId) + 1;
@@ -33,12 +38,12 @@ public class ProductDAOStub : IDAO<Product>
     {
         throw new NotImplementedException();
     }
-
+    //Returnere hele listen af produkter
     public IEnumerable<Product> GetAll()
     {
         return _products;
     }
-
+    //I produkt listen, kigger vi efter medsendte id, der kigges igennem alle produkters id indtil der findes et der matcher det medsendte. 
     public Product GetById(int id)
     {
         return _products.First(product => product.ProductId == id);
