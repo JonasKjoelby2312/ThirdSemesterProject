525ed040b3be339bb91cf08e052535ae034ff5c1
ChristianBjerreSkadhauge <150657731+ChristianBjerreSkadhauge@users.noreply.github.com>
2024-11-20 10:02:32 +0100

    WebAPI/ Program (hildur.ucn.dk)

--- ThirdSemesterProject.WebAPI/Program.cs
+++ ThirdSemesterProject.WebAPI/Program.cs
@@ -20,10 +20,10 @@ namespace ThirdSemesterProject.WebAPI
             /*builder.Services.AddSingleton<IDAO<Product>, ProductDAOStub>();*/
 
             //Docker ConnectionString: 
-            const string connectionString = "Server=tcp:localhost,1433;Database=webshop;User ID=sa;Password=Dockerstrongpwd123;";
+            const string connectionString = "Server=tcp:hildur.ucn.dk,1433;Database=DMA-CSD-S232_10503126;User ID=DMA-CSD-S232_10503126;Password=Password1!;";
 
             //SSMS connectionString: 
-            //const string connectionString = "Data Source=.;Initial Catalog=webshop;Integrated Security=True";
+            /*const string connectionString = "Data Source=.;Initial Catalog=webshop;Integrated Security=True";*/
             builder.Services.AddSingleton<IDAOAsync<Product>>((_) => (IDAOAsync<Product>) new ProductDAO(connectionString));
             builder.Services.AddSingleton<IDAOAsync<SaleOrder>>((_) => (IDAOAsync<SaleOrder>)new SaleOrderDAO(connectionString));
             var app = builder.Build();
