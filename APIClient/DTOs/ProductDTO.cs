bab51a22f6e5f9e0dbf2191c76c408ca475a72b5
JonasKjoelby2312 <150658707+JonasKjoelby2312@users.noreply.github.com>
2024-11-20 09:27:24 +0100

    Product/ProductDTO ændring til samme attributter.

--- APIClient/DTOs/ProductDTO.cs
+++ APIClient/DTOs/ProductDTO.cs
@@ -13,11 +13,16 @@ public class ProductDTO
     public int ProductId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string Size { get; set; }
     public decimal Weight { get; set; }
-    public decimal SalesPrice { get; set; }
+    public string Size { get; set; }
     public int CurrentStock { get; set; }
+    public decimal SalesPrice { get; set; }
+    public string Color { get; set; }
     public string ProductType { get; set; }
+
+    
+
+
     #endregion
 
     public ProductDTO()
