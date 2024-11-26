318e8b1893a6e85c430dda9ad80ab96ae7258093
JonasKjoelby2312 <150658707+JonasKjoelby2312@users.noreply.github.com>
2024-11-26 08:43:24 +0100

    Fjernelse af IMapper

--- ThirdSemesterProject.WebAPI/Program.cs
+++ ThirdSemesterProject.WebAPI/Program.cs
@@ -17,7 +17,7 @@ namespace ThirdSemesterProject.WebAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddAutoMapper(typeof(CustomerProfile));
+            
             /*builder.Services.AddSingleton<IDAO<Product>, ProductDAOStub>();*/
 
             //Docker ConnectionString: 
