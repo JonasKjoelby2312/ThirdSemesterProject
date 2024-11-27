e8e48c8d2372f0ed0d81d2f82a5207ee28eef5a3
AugustPeterNielsen <150657560+AugustPeterNielsen@users.noreply.github.com>
2024-11-27 12:30:23 +0100

    PRODUCT DAO QUERIES FIXED

--- ThirdSemesterProject.DAL/DAOs/ProductDAO.cs
+++ ThirdSemesterProject.DAL/DAOs/ProductDAO.cs
@@ -5,18 +5,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.VisualBasic;
 using ThirdSemesterProject.DAL.Model;
 
 namespace ThirdSemesterProject.DAL.DAOs;
 
 public class ProductDAO : BaseDAO, IDAOAsync<Product>
 {
-    private readonly string GET_PRODUCT_WITH_NEWEST_SALES_PRICE = "WITH LatestSalesPrice AS(SELECT fk_product_id, value, creation_date FROM sales_price WHERE creation_date = (SELECT MAX(creation_date) FROM sales_price WHERE fk_product_id = sales_price.fk_product_id)) SELECT product_id AS productId, name, description, weight, size, color, current_stock AS currentStock, value AS salesPrice FROM product JOIN LatestSalesPrice ON product.product_id = LatestSalesPrice.fk_product_id;";
-    private readonly string GET_ALL_PRODUCTS = "SELECT product_id, name, description, weight, size, color, current_stock from product;";
+    private readonly string GET_PRODUCT_WITH_NEWEST_SALES_PRICE = "WITH LatestSalesPrice AS (SELECT fk_product_id, value, creation_date FROM sales_price sp WHERE sales_price_id = (SELECT TOP 1 sales_price_id FROM sales_price WHERE fk_product_id = sp.fk_product_id ORDER BY creation_date DESC, sales_price_id DESC)) SELECT product.product_id AS productId, product.name, product.description, product.weight, product.size, product.color, product.current_stock AS currentStock, LatestSalesPrice.value AS salesPrice, product.product_type AS productType FROM product LEFT JOIN LatestSalesPrice ON product.product_id = LatestSalesPrice.fk_product_id;";
+    private readonly string GET_ALL_PRODUCTS = "SELECT product_id, name, description, weight, size, color, current_stock, product_type from product;";
     private readonly string INSERT_PRODUCT = "INSERT INTO product(name, description, weight, size, color, current_stock, product_type) values (@name, @description, @weight, @size, @color, @currentStock, @productType) SELECT CAST(SCOPE_IDENTITY() AS INT)";
     //private readonly string INSERT_COMPONENT = "INSERT INTO component name = @name, description = @description, weight = @weight;";
-    private readonly string SELECT_PRODUCT_BY_ID = "WITH LatestSalesPrice AS(SELECT fk_product_id, value, creation_date FROM sales_price WHERE creation_date = (SELECT MAX(creation_date) FROM sales_price WHERE fk_product_id = sales_price.fk_product_id)) SELECT product_id AS productId, name, description, weight, size, color, current_stock AS currentStock, value AS salesPrice FROM product JOIN LatestSalesPrice ON product.product_id = LatestSalesPrice.fk_product_id WHERE product_id = @productId;";
-    
+    private readonly string SELECT_PRODUCT_BY_ID = "WITH LatestSalesPrice AS(SELECT fk_product_id, value, creation_date FROM sales_price WHERE creation_date = (SELECT MAX(creation_date) FROM sales_price WHERE fk_product_id = sales_price.fk_product_id)) SELECT product_id AS productId, name, description, weight, size, color, current_stock AS currentStock, product_type AS productType, value AS salesPrice FROM product JOIN LatestSalesPrice ON product.product_id = LatestSalesPrice.fk_product_id WHERE product_id = @productId;";
+    private readonly string INSERT_SALES_PRICE = "insert into sales_price (creation_date, [value], fk_product_id) VALUES (@creationDate, @value, @fkProductId)";
 
     public ProductDAO(string connectionString) : base(connectionString)
     {
@@ -26,15 +27,18 @@ public class ProductDAO : BaseDAO, IDAOAsync<Product>
     {
         using var connection = CreateConnection();
         connection.Open();
-
+        IDbTransaction transaction = connection.BeginTransaction();
         try
         {
-            int productId = await connection.ExecuteScalarAsync<int>(INSERT_PRODUCT, entity);
+            int productId = await connection.ExecuteScalarAsync<int>(INSERT_PRODUCT, entity, transaction);
+            await connection.ExecuteAsync(INSERT_SALES_PRICE, new { value = entity.SalesPrice, creationDate = DateTime.Now, fkProductId = productId }, transaction);
 
+            transaction.Commit();
             return productId;
         }
         catch (Exception ex)
         {
+            transaction.Rollback();
             throw new Exception($"Error inserting new product: '{ex.Message}'.", ex);
         }
     }
