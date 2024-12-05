a0813f0ab1898a39229ad4ff683126e4fc9adf05
malt3077 <10503077@ucn.dk>
2024-12-05 11:30:08 +0100

    Login fix

--- ThirdSemesterProject.WebAPI/DTOs/PersonDTO.cs
+++ ThirdSemesterProject.WebAPI/DTOs/PersonDTO.cs
@@ -6,7 +6,6 @@ public class PersonDTO
 {
     public int PersonId { get; set; }
     public string? Name { get; set; }
-    
     public string Email { get; set; }
     public string? PhoneNO { get; set; }
     public string PersonType { get; set; }
