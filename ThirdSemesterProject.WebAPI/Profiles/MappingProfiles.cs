e207424a0e9945efb62908503d61d1ad132c6890
JonasKjoelby2312 <150658707+JonasKjoelby2312@users.noreply.github.com>
2024-12-05 09:36:09 +0100

    Update MappingProfiles.cs

--- ThirdSemesterProject.WebAPI/Profiles/MappingProfiles.cs
+++ ThirdSemesterProject.WebAPI/Profiles/MappingProfiles.cs
@@ -8,7 +8,7 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-        CreateMap<Customer, CustomerDTO>().ReverseMap().ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.AddressDTO)) ;
+        CreateMap<Customer, CustomerDTO>().ReverseMap().ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.AddressDTO));
         CreateMap<Address, AddressDTO>().ReverseMap();  
         
         
