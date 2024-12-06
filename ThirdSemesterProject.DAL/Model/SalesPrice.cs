f23cf24eb8b035b10627ea5b61609a35bc1534a8
ChristianBjerreSkadhauge <150657731+ChristianBjerreSkadhauge@users.noreply.github.com>
2024-12-06 08:51:52 +0100

    SaleOrderTestDAO

--- ThirdSemesterProject.DAL/Model/SalesPrice.cs
+++ ThirdSemesterProject.DAL/Model/SalesPrice.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace ThirdSemesterProject.DAL.Model;
+    namespace ThirdSemesterProject.DAL.Model;
 
-public class SalesPrice
-{
-    public required DateTime CreationDate { get; set; }
-    public required decimal Value { get; set; }
-
-    public SalesPrice(DateTime creationDate, decimal value)
+    public class SalesPrice
     {
-        CreationDate = creationDate;
-        Value = value;
-    }
+        public required DateTime CreationDate { get; set; }
+        public required decimal Value { get; set; }
+
+        public SalesPrice(DateTime creationDate, decimal value)
+        {
+            CreationDate = creationDate;
+            Value = value;
+        }
+    
 }
--- ThirdSemesterProject.NUnitTest/SaleOrderDAOTests.cs
+++ ThirdSemesterProject.NUnitTest/SaleOrderDAOTests.cs
@@ -58,22 +58,22 @@ public class SaleOrderDAOTests
         _saleOrderDAO = new SaleOrderDAO("Server=tcp:hildur.ucn.dk,1433;Database=DMA-CSD-S232_10503126;User ID=DMA-CSD-S232_10503126;Password=Password1!;");
 
         Customer customer = new Customer() {Name = "Dude", Email = "Dude@gmail.com", Address = new Address() {RoadName = "dudestreet", City = "DudeCity", HouseNo = "69", Zip = 9999 } };
-        SaleOrder _saleOrder = new SaleOrder(customer);
-        Product product1 = new Product() { Name = "fack", Color = "redfack", CurrentStock = 2, Description = "fack is fack", ProductType = "Equipment", SalesPrice = new SalesPrice() { CreationDate = DateTime, Value = 2 }, Size = "L",  };
-        _saleOrder.AddOrderLineToSaleOrder(new OrderLine(product1, 12));
+        _saleOrder = new SaleOrder();
+        Product product = new Product() { Name = "fack", Color = "redfack", CurrentStock = 2, Description = "fack is fack", ProductType = "Equipment", SalesPrice = new SalesPrice() { CreationDate = DateTime.Now, Value = 2.2m }, Size = "L", Weight = 2 };
+        _saleOrder.AddOrderLineToSaleOrder(new OrderLine { Product = product, Quantity = 12, UnitPrice = 2 });
+
     }
 
     [Test]
     public async Task TestSaleOrderCreateAsync()
     {
         //Arrange
-        
-
+        var expectedOrderId = await _saleOrderDAO.CreateAsync(_saleOrder);
         //Act
 
         //Assert
 
-        Assert.DoesNotThrowAsync(async () => await saleOrderDAO.CreateAsync(_saleOrder));
+        Assert.That(_saleOrder.SaleOrderId > 0);
     }
 
 
