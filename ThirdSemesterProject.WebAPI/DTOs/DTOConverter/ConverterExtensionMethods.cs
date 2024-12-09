e32db562adf8e1ded06681e6b4e47ad31175b494
AugustPeterNielsen <150657560+AugustPeterNielsen@users.noreply.github.com>
2024-12-09 11:46:06 +0100

    Doc for Converter methods

--- ThirdSemesterProject.WebAPI/DTOs/DTOConverter/ConverterExtensionMethods.cs
+++ ThirdSemesterProject.WebAPI/DTOs/DTOConverter/ConverterExtensionMethods.cs
@@ -2,8 +2,22 @@
 
 namespace ThirdSemesterProject.WebAPI.DTOs.DTOConverter;
 
+/// <summary>
+/// Provides extension methods for copying properties between objects.
+/// </summary>
 public static class ConverterExtensionMethods
 {
+    /// <summary>
+    /// Copies properties from the source object to the destination object if the property names and types match.
+    /// </summary>
+    /// <typeparam name="T">The type of the destination object.</typeparam>
+    /// <param name="sourceObject">The source object from which properties are copied.</param>
+    /// <param name="destinationObject">The destination object to which properties are copied.</param>
+    /// <returns>The destination object with updated property values.</returns>
+    /// <remarks>
+    /// - Only properties with matching names and types between the source and destination objects are copied.
+    /// - Properties in the destination object that are not writable are ignored.
+    /// </remarks>
     public static T CopyPropertiesTo<T>(this object sourceObject, T destinationObject)
     {
         foreach (PropertyInfo destinationProperty in destinationObject.GetType().GetProperties().Where(p => p.CanWrite))
--- ThirdSemesterProject.WebAPI/DTOs/DTOConverter/DTOConverter.cs
+++ ThirdSemesterProject.WebAPI/DTOs/DTOConverter/DTOConverter.cs
@@ -3,8 +3,17 @@ using ThirdSemesterProject.DAL.Model;
 
 namespace ThirdSemesterProject.WebAPI.DTOs.DTOConverter;
 
+/// <summary>
+/// Provides extension methods to convert between DTO (Data Transfer Objects).
+/// </summary>
 public static class DTOConverter
 {
+
+    /// <summary>
+    /// Converts a <see cref="SaleOrderDTO"/> to a <see cref="SaleOrder"/>.
+    /// </summary>
+    /// <param name="saleOrderDTOToConvert">The DTO to convert.</param>
+    /// <returns>A <see cref="SaleOrder"/></returns>
     public static SaleOrder FromDTO(this SaleOrderDTO saleOrderDTOToConvert )
     {
         var saleOrder = new SaleOrder();
@@ -19,6 +28,12 @@ public static class DTOConverter
         return saleOrder;
     }
 
+    /// <summary>
+    /// Converts an <see cref="OrderLineDTO"/> to an <see cref="OrderLine"/> with a given product.
+    /// </summary>
+    /// <param name="orderLineDTOToConvert">The DTO to convert.</param>
+    /// <param name="product">The associated <see cref="Product"/>.</param>
+    /// <returns>An <see cref="OrderLine"/></returns>
     public static OrderLine FromDTO(this OrderLineDTO orderLineDTOToConvert, Product product)
     {
         var orderLine = new OrderLine();
@@ -27,6 +42,11 @@ public static class DTOConverter
         return orderLine;
     }
 
+    /// <summary>
+    /// Converts a <see cref="ProductDTO"/> to a <see cref="Product"/>.
+    /// </summary>
+    /// <param name="productDTOToConvert">The DTO to convert.</param>
+    /// <returns>A <see cref="Product"/></returns>
     public static Product FromDTO(this ProductDTO productDTOToConvert)
     {
         var product = new Product();
@@ -34,6 +54,11 @@ public static class DTOConverter
         return product;
     }
 
+    /// <summary>
+    /// Converts a <see cref="CustomerDTO"/> to a <see cref="Customer"/>.
+    /// </summary>
+    /// <param name="customerDTOToConvert">The DTO to convert.</param>
+    /// <returns>A <see cref="Customer"/></returns>
     public static Customer FromDTO(this CustomerDTO customerDTOToConvert)
     {
         var customer = new Customer();
@@ -41,30 +66,48 @@ public static class DTOConverter
         return customer;
     }
 
+    /// <summary>
+    /// Converts a <see cref="SaleOrder"/>to a <see cref="SaleOrderDTO"/>.
+    /// </summary>
+    /// <param name="saleOrderToConvert">to convert.</param>
+    /// <returns>A <see cref="SaleOrderDTO"/>.</returns>
     public static SaleOrderDTO ToDTO(this SaleOrder saleOrderToConvert)
     {
         var saleOrderDTO = new SaleOrderDTO();
         saleOrderToConvert.CopyPropertiesTo(saleOrderDTO);
         return saleOrderDTO;
-
     }
+
+    /// <summary>
+    /// Converts an enumerable of <see cref="SaleOrder"/> to a collection of <see cref="SaleOrderDTO"/>s.
+    /// </summary>
+    /// <param name="saleOrderToConvert">The enumerable to convert.</param>
+    /// <returns>An enumerable of <see cref="SaleOrderDTO"/>s.</returns>
     public static IEnumerable<SaleOrderDTO> ToDTOs(this IEnumerable<SaleOrder> saleOrderToConvert)
     {
         foreach (var saleOrder in saleOrderToConvert)
         {
             yield return saleOrder.ToDTO();
         }
-
     }
 
+    /// <summary>
+    /// Converts an <see cref="OrderLineWithProducts"/> to an <see cref="OrderLineWithProductsDTO"/>.
+    /// </summary>
+    /// <param name="orderLineWithProducts"> to convert.</param>
+    /// <returns>An <see cref="OrderLineWithProductsDTO"/>.</returns>
     private static OrderLineWithProductsDTO ToDTO(this OrderLineWithProducts orderLineWithProducts)
     {
         var orderLineWithProductDTO = new OrderLineWithProductsDTO();
         orderLineWithProducts.CopyPropertiesTo(orderLineWithProductDTO);
         return orderLineWithProductDTO;
-
     }
 
+    /// <summary>
+    /// Converts an enumerable of <see cref="OrderLineWithProducts"/> to a collection of <see cref="OrderLineWithProductsDTO"/>s.
+    /// </summary>
+    /// <param name="orderLineWithProducts">The enumerable to convert.</param>
+    /// <returns>An enumerable of <see cref="OrderLineWithProductsDTO"/>s.</returns>
     public static IEnumerable<OrderLineWithProductsDTO> ToDTOs(this IEnumerable<OrderLineWithProducts> orderLineWithProducts)
     {   
 
@@ -72,7 +115,5 @@ public static class DTOConverter
         {
             yield return olwp.ToDTO();
         }
-        
-
     }
 }
