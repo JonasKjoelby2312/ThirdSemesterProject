91ccd1fac901ccc2a8f1dd922237255d14cf67b0
JonasKjoelby2312 <150658707+JonasKjoelby2312@users.noreply.github.com>
2024-12-16 09:51:07 +0100

    Væk med udkommenterede kode

--- ThirdSemesterProject.WebSite/Controllers/CustomersController.cs
+++ ThirdSemesterProject.WebSite/Controllers/CustomersController.cs
@@ -73,29 +73,7 @@ public class CustomersController : Controller
     {
         var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
         var authPropterties = new AuthenticationProperties
-        {
-            #region often used options - to consider including in cookie
-            //AllowRefresh = <bool>,
-            // Refreshing the authentication session should be allowed.
-
-            //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-            // The time at which the authentication ticket expires. A 
-            // value set here overrides the ExpireTimeSpan option of 
-            // CookieAuthenticationOptions set with AddCookie.
-
-            //IsPersistent = true,
-            // Whether the authentication session is persisted across 
-            // multiple requests. When used with cookies, controls
-            // whether the cookie's lifetime is absolute (matching the
-            // lifetime of the authentication ticket) or session-based.
-
-            //IssuedUtc = <DateTimeOffset>,
-            // The time at which the authentication ticket was issued.
-
-            //RedirectUri = <string>
-            // The full path or absolute URI to be used as an http 
-            // redirect response value. 
-            #endregion
+        {   
         };
         await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authPropterties);
     }
